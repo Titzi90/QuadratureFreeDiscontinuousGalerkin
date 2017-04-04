14bd08971797c8c43fff0829c2a22f2ed2154246
Christopher Bross <christopher.bross@fau.de>
2017-04-04 16:21:45 +0200

    first simple polynomial class

---
+++ code/polynomial.hpp
@@ -0,0 +1,147 @@
+#ifndef POLYNOMIAL_HPP
+#define POLYNOMIAL_HPP
+
+#include <vector>
+#include <ostream>
+#include <limits>
+#include <string>
+#include <sstream>
+#include <cassert>
+#include <algorithm>
+#include <iterator>
+
+/**
+ * Class holding an 2 dimensional polynomial of order 'order'
+ */
+class Polynomial2D
+{
+public:
+  // Constructor getting the order (-> size) of the polynomial
+  Polynomial2D(unsigned int order)
+    :order_(order), coeficents_(( order_+1 )*( order_+1 ), 0.)
+  {}
+  // Constructor for simple polynomials with one coefficient non equal to 0
+  Polynomial2D(unsigned int xEx, unsigned int yEx, double coeficent)
+    :order_(xEx+yEx), coeficents_((order_+1)*(order_+1),0.)
+  {
+    (*this)(xEx,yEx) = coeficent;
+  }
+
+  // geter for the order of the polynomial
+  unsigned int getOrder() const {return order_;}
+
+  // getter / setter for coefficients
+  double const & operator()(unsigned int xExponent, unsigned int yExponent) const
+  {
+    // assert (xExponent + yExponent <= order_);
+    return coeficents_[xExponent * (order_+1) + yExponent];
+  }
+  double& operator() (unsigned int xExponent, unsigned int yExponent)
+  {
+    // assert (xExponent + yExponent <= order_);
+    return coeficents_[xExponent * (order_+1) + yExponent];
+  }
+
+private:
+  unsigned int const order_;
+  std::vector<double> coeficents_;
+
+  // friends declarations
+  friend auto begin(Polynomial2D & pol)      ->decltype(coeficents_.begin());
+  friend auto begin(Polynomial2D const & pol)->decltype(const_cast<const std::vector<double>&>(coeficents_).begin());
+  friend auto end(Polynomial2D & pol)        ->decltype(coeficents_.end());
+  friend auto end(Polynomial2D const & pol)  ->decltype(const_cast<const std::vector<double>&>(coeficents_).end());
+};
+
+template <class BinaryOperation>
+Polynomial2D operatorPlusMinusHelper(Polynomial2D const & lhs, Polynomial2D const & rhs, BinaryOperation op)
+{
+  Polynomial2D result (lhs);
+  for (unsigned int x=0; x<=rhs.getOrder(); ++x)
+    for (unsigned int y=0; y<=rhs.getOrder(); ++y)
+      result(x,y) = op(result(x,y), rhs(x,y));
+  return result;
+}
+
+// plus operator for polynomials
+Polynomial2D operator+(Polynomial2D const & lhs, Polynomial2D const & rhs)
+{
+  return lhs.getOrder()>rhs.getOrder() ? operatorPlusMinusHelper(lhs, rhs, std::plus<double>())
+                                       : operatorPlusMinusHelper(rhs, lhs, std::plus<double>());
+}
+Polynomial2D operator+(Polynomial2D const & lhs, double rhs){ return lhs + Polynomial2D(0,0,rhs); }
+Polynomial2D operator+(double lhs, Polynomial2D const & rhs){ return rhs+lhs; }
+
+// multiplication operator for polynomials
+Polynomial2D operator*(Polynomial2D const & lhs, Polynomial2D const & rhs)
+{
+  Polynomial2D result = Polynomial2D(lhs.getOrder()+rhs.getOrder());
+
+  //loop over lhs
+  for (unsigned int lhs_x=0; lhs_x<=lhs.getOrder(); ++lhs_x)
+    for (unsigned int lhs_y=0; lhs_y<=lhs.getOrder(); ++lhs_y)
+    {
+      double lhs_coefficent = lhs(lhs_x, lhs_y);
+      if (0 == lhs_coefficent)
+        continue;
+      //loop over rhs
+      for (unsigned int rhs_x=0; rhs_x<=rhs.getOrder(); ++rhs_x)
+        for (unsigned int rhs_y=0; rhs_y<=rhs.getOrder(); ++rhs_y)
+        {
+          double rhs_coefficent = rhs(rhs_x, rhs_y);
+          if (0 == rhs_coefficent)
+            continue;
+          result(lhs_x+rhs_x, lhs_y+rhs_y) += lhs_coefficent*rhs_coefficent;
+        }
+    }
+  return result;
+}
+Polynomial2D operator*(double const lhs, Polynomial2D const & rhs){
+  Polynomial2D result = Polynomial2D(rhs.getOrder());
+  std::transform(begin(rhs), end(rhs), begin(result),
+                 [lhs](decltype(*begin(rhs)) x){ return lhs * x; });
+  return result;
+}
+Polynomial2D operator*(Polynomial2D const & lhs, double rhs){ return rhs*lhs; }
+
+// minus operator for polynomials
+Polynomial2D operator-(Polynomial2D const & lhs, Polynomial2D const & rhs){ return lhs + (-1*rhs); }
+Polynomial2D operator-(Polynomial2D const & lhs, double rhs){ return lhs-Polynomial2D(0,0,rhs); }
+Polynomial2D operator-(double lhs, Polynomial2D const & rhs){ return Polynomial2D(0,0,lhs)-rhs; }
+
+//TODO division
+
+// iterator access functions
+auto begin(Polynomial2D & pol)      ->decltype(pol.coeficents_.begin()){ return pol.coeficents_.begin(); }
+auto begin(Polynomial2D const & pol)->decltype(pol.coeficents_.begin()){ return pol.coeficents_.begin(); }
+auto end(Polynomial2D & pol)        ->decltype(pol.coeficents_.end())  { return pol.coeficents_.end(); }
+auto end(Polynomial2D const & pol)  ->decltype(pol.coeficents_.end())  { return pol.coeficents_.end(); }
+
+// converting a polynomial to a string
+std::string to_string(Polynomial2D const & pol)
+{
+  std::stringstream ss;
+  unsigned int order = pol.getOrder();
+
+  for(unsigned int x=order; x!=std::numeric_limits<unsigned int>::max(); --x)
+    {
+      for(unsigned int y=order-x; y!=std::numeric_limits<unsigned int>::max(); --y)
+        {
+          if(0 != pol(x,y))
+            {
+              //TODO plus nur wenn ned minus
+              ss << pol(x,y) << "x^" << x << "y^" << y << " + ";
+            }
+        }
+    }
+  std::string str = ss.str();
+  return str.erase(str.size()-3);
+}
+
+// output operator for polynomials
+std::ostream& operator<< (std::ostream& os, Polynomial2D const & pol)
+{
+  os << to_string(pol) << std::endl;
+  return os;
+}
+#endif
---
+++ code/test.cpp
@@ -0,0 +1,78 @@
+#include "polynomial.hpp"
+#include <iostream>
+
+int test(Polynomial2D pol, std::string pol_str, std::string msg)
+{
+  if ( pol_str != to_string(pol)){
+    std::cerr << "Polynomial2D Class has an error!\n" << msg
+              << "\n\tExpackted polynom: " << pol_str
+              << "\n\tgot: " << to_string(pol)
+              << std::endl;
+    return -1;
+  }
+  return 0;
+}
+int test(Polynomial2D pol1, Polynomial2D pol2, std::string msg){ return test(pol1, to_string(pol2), msg); }
+
+
+int main()
+{
+  int status = 0;
+
+  // Test create 1
+  Polynomial2D pol1 = Polynomial2D(2);
+  pol1(2,0) = 3.2;
+  pol1(1,1) = 2.2;
+  pol1(1,0) = 1.2;
+  pol1(0,2) = -2.2;
+  pol1(0,1) = -1.2;
+  pol1(0,0) = 5.2;
+  std::string const pol1_str ("3.2x^2y^0 + 2.2x^1y^1 + 1.2x^1y^0 + -2.2x^0y^2 + -1.2x^0y^1 + 5.2x^0y^0");
+  status = status || test(pol1, pol1_str, "creating complex 2D polynomial");
+
+  // Test create 2
+  Polynomial2D pol2 = Polynomial2D(2);
+  pol2(2,0) = -2;
+  pol2(1,1) = 3;
+  std::string const pol2_str ("-2x^2y^0 + 3x^1y^1");
+  status = status || test(pol2, pol2_str, "creating complex 2D polynomial");
+
+  // Test create 2
+  Polynomial2D pol3 = Polynomial2D(2,1,-2.5);
+  std::string const pol3_str ("-2.5x^2y^1");
+  status = status || test(pol3, pol3_str, "creating simple 2D polynomial");
+
+  // Test plus
+  Polynomial2D pol4 = pol1 + pol2;
+  std::string const pol4_str ("1.2x^2y^0 + 5.2x^1y^1 + 1.2x^1y^0 + -2.2x^0y^2 + -1.2x^0y^1 + 5.2x^0y^0");
+  status = status || test(pol4, pol4_str, "add two polynomials");
+
+  // Test plus of different orders
+  Polynomial2D pol5 = pol2 + pol3;
+  std::string const pol5_str ("-2.5x^2y^1 + -2x^2y^0 + 3x^1y^1");
+  status = status || test(pol5, pol5_str, "add two polynomials of different order");
+
+  // Test plus real
+  Polynomial2D pol6 = pol5 + 3.7;
+  std::string const pol6_str ("-2.5x^2y^1 + -2x^2y^0 + 3x^1y^1 + 3.7x^0y^0");
+  status = status || test(pol6, pol6_str, "add real number to polynomial");
+
+  // Test ...
+  // Polynomial2D pol7 =
+
+  // Test Multiplication
+  Polynomial2D pol8 = pol1 * pol2;
+  std::string const pol8_str ("-6.4x^4y^0 + 5.2x^3y^1 + -2.4x^3y^0 + 11x^2y^2 + 6x^2y^1 + -10.4x^2y^0 + -6.6x^1y^3 + -3.6x^1y^2 + 15.6x^1y^1");
+  status = status || test(pol8, pol8_str, "multiply two polynomials");
+
+  // Test scale
+  Polynomial2D pol9 = pol1 * (-1);
+  std::string const pol9_str ("-3.2x^2y^0 + -2.2x^1y^1 + -1.2x^1y^0 + 2.2x^0y^2 + 1.2x^0y^1 + -5.2x^0y^0");
+  status = status || test(pol9, pol9_str, "multiply real with polynomial (scale)");
+
+  // Test minus
+  Polynomial2D pol10 = pol2 - pol5;
+  status = status || test(pol10, -1*pol3, "subtract two polynomials");
+
+  return status;
+}
